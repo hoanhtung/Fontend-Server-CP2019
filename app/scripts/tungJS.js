3d1c5b262c40938b29a118cd29aa9ab043e0be73
HOANH-TUNG\mastermind <hoanhtung>
2019-02-21 11:15:38 +0700

    first commit

---
+++ app/scripts/tungJS.js
@@ -0,0 +1,74 @@
+//Get room show UI
+var EBSMSLocal = 'https://localhost:44372';
+function loadSurgeryRoom(surgeryDay) {
+    var strAppend1 = '';
+    var divRoom = $('#row-surgery-room');
+    divRoom.empty();
+    $.ajax({
+        url: EBSMSLocal + '/api/Schedule/GetSurgeryRooms/',
+        method: 'get',
+        success: function(room) {
+            for (let index = 0; index < room.length; index++) {
+                strAppend1 += '<div class="div-roomHeaderItem"><span>' 
+                + room[index].name + '</span><div id ="header-room-' + room[index].id + '"></div>';
+                $.ajax({
+                    url: EBSMSLocal + '/api/Schedule/GetSurgeryShiftsByRoomAndDate/',
+                    method: 'get',
+                    data: { roomId: room[index].id, dayNumber: surgeryDay},
+                    success: function(shift) {
+                        var strAppend2 = '';
+                        for (let index = 0; index < shift.length; index++) {
+                            strAppend2 += '<a href="./viewScheduleItem.html"><div class="div-roomBodyItem">' +
+                            // 'Surgeon:' + 'Nguyễn Hoàng Anh' +
+                            '<div><b>' + shift[index].catalogName + '</b></div>' +
+                            '<div><b>Patient:</b> ' +  shift[index].patientName + '</div>' +
+                            '<div><b>Time:</b> ' + shift[index].estimatedStartDateTime + ' - ' + shift[index].estimatedEndDateTime + '</div>' +
+                            '</div></a>';
+                        }
+                        $('#header-room-' + room[index].id).append(strAppend2);
+                    }
+                });
+                strAppend1 += '</div>';
+            }
+            divRoom.append(strAppend1);
+        }
+    });
+}
+function LoadSurgeryShiftByRoomAndDate() {
+    $.ajax({
+        url: EBSMSLocal + '/api/Schedule/GetSurgeryShiftsByRoomAndDate/',
+        method: 'get',
+        data: { roomId: room[index].id, dayNumber: surgeryDay},
+        success: function(shift) {
+            console.log(shift);
+            for (let index = 0; index < shift.length; index++) {
+                strAppend += '<a href="./viewScheduleItem.html"><div class="div-roomBodyItem">' +
+                // 'Surgeon:' + 'Nguyễn Hoàng Anh' +
+                // 'Phẫu thuật xương' +
+                shift[index].catelogName +
+                'Patient: ' +  shift[index].patientName +
+                'Time: ' + shift[index].estimatedStartDateTime + '-' + shift[index].estimatedEndDateTime;
+                '</div></a>';
+            }
+        }
+    });
+}
+function getScheduleByDay() {
+    var date = new Date($('#date-input').val());
+    loadSurgeryRoom(convertDateToNumber(date));
+}
+function convertDateToNumber(date) {
+    var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    var month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
+    var year = date.getFullYear();
+    var dateNumber = [year, month, day].join('');
+    return dateNumber;
+}
+function formatInputDate(date) {
+    var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    var month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
+    var year = date.getFullYear();
+    var dateString = [year, month, day].join('-');
+    return dateString;
+}
+ 
