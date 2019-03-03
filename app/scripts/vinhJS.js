0faea3f890adbb36fde973cf1e0a2d1bfbac7b3f
cvin59 <cvin59@gmail.com>
2019-03-04 03:08:59 +0700

    Update vinhJS.js

--- app/scripts/vinhJS.js
+++ app/scripts/vinhJS.js
@@ -13,7 +13,7 @@ function createTreatmentReport(shiftId, progressiveDisease, medicalRequirement) 
         .done(rs => {
             console.log(rs)
             loadTreatmentReport(shiftId);
-            $('#treatmentModal').modal('toggle'); 
+            $('#treatmentModal').modal('toggle');
         })
         .fail(er => console.log(er));
 }
@@ -60,19 +60,30 @@ function loadHealthcareReport(shiftId) {
             console.table(data);
             if (data.length != 0) {
                 var container = '<table class="table "><thead><tr><th>No.</th><th>Create Time</th>'
+                    + '<th>Wound Condition</th>'
                     + '<th>Event Content</th><th>Care Content</th></tr></thead>';
                 for (var i = 0; i < data.length; i++) {
                     var progressiveDisease = data[i].eventContent.replace(/\n/g, "<br />");
                     var medicalRequirement = data[i].careContent.replace(/\n/g, "<br />");
+                    var badgeWoundCondition;
+                    switch ( data[i].woundCondition) {
+                        case 1:
+                            badgeWoundCondition = '<span class="badge badge-success">Good</span>';
+                            break;
+                        case 2:
+                            badgeWoundCondition = '<span class="badge badge-danger">Bad</span>';
+                            break;
+                    }
                     container += '<tr><td>' + (i + 1) + '</td>'
                     container += '<td>' + data[i].dateCreated.split('T')[0] + '</td>'
+                        + `<td>${badgeWoundCondition}</td>`
                         + '<td>' + progressiveDisease + '</td>'
                         + '<td>' + medicalRequirement + '</td></tr>';
                 }
                 container += '</table>';
                 div_healthcare_table.append(container);
             } else {
-                div_healthcare_table.append('<h2>Not found no treatment</h2>');
+                div_healthcare_table.append('<h2>Not found no healthcare report</h2>');
             }
 
         },
